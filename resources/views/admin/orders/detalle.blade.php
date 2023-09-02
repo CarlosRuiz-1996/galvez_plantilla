fb55ff86042b667cc1f6b0f51bb008f048727f86
Carlos Ruiz <131738990+CarlosRuiz-1996@users.noreply.github.com>
2023-09-01 18:52:34 -0600

    cambiar diseño de detalle de productos

--- resources/views/admin/orders/detalle.blade.php
+++ resources/views/admin/orders/detalle.blade.php
@@ -1,68 +1,49 @@
+    <table class="w-full divide-y divide-gray-400">
+        <thead class="bg-orange-100">
+            <tr>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    producto
+                </th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    costo
+                </th>
+                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    cantidad
+                </th>
+            </tr>
+        </thead>
+        <tbody class="bg-white divide-y divide-gray-200">
+            @php
+                $total = 0; // Inicializa el total
+            @endphp
 
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Detalle de pedido') }}
-        </h2>
-    </x-slot>
+            @foreach ($detallePedidos as $detalle)
+                <tr>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                        {{ $detalle->product->descripcion ?? 'N/A' }}
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                        {{ $detalle->product->price ?? 'N/A' }}
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                        {{ $detalle->amount }}
+                    </td>
+                </tr>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <x-alert />
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <h2 class="text-lg font-semibold mb-4">Detalles del pedido</h2>
-                    <div class="overflow-x-auto">
-                        <table class="w-full divide-y divide-gray-200">
-                            <thead class="bg-gray-50">
-                                <tr>
-                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        producto
-                                    </th>
-                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        costo
-                                    </th>
-                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                        cantidad
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody class="bg-white divide-y divide-gray-200">
-                                @php
-                                    $total = 0; // Inicializa el total
-                                @endphp
-                                
-                                @foreach ($detallePedidos as $detalle)
-                                    <tr>
-                                        <td class="px-6 py-4 whitespace-nowrap">
-                                            {{ $detalle->product->descripcion ?? 'N/A' }}
-                                        </td>
-                                        <td class="px-6 py-4 whitespace-nowrap">
-                                            {{ $detalle->product->price ?? 'N/A' }}
-                                        </td>
-                                        <td class="px-6 py-4 whitespace-nowrap">
-                                            {{ $detalle->amount }}
-                                        </td>
-                                    </tr>
-                                    
-                                    @php
-                                        $subtotal = $detalle->product->price * $detalle->amount; // Calcula el subtotal
-                                        $total += $subtotal; // Suma el subtotal al total
-                                    @endphp
-                                @endforeach
-                                
-                                <!-- Fila para mostrar el total -->
-                                <tr>
-                                    <td class="px-6 py-4 whitespace-nowrap font-semibold" colspan="2">
-                                        Total:
-                                    </td>
-                                    <td class="px-6 py-4 whitespace-nowrap font-semibold">
-                                        {{ $total }}
-                                    </td>
-                                </tr>
-                            </tbody>                      
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+                @php
+                    $subtotal = $detalle->product->price * $detalle->amount; // Calcula el subtotal
+                    $total += $subtotal; // Suma el subtotal al total
+                @endphp
+            @endforeach
+
+            <!-- Fila para mostrar el total -->
+            <tr>
+                <td class="px-6 py-4 whitespace-nowrap font-semibold" colspan="2">
+                    Total:
+                </td>
+                <td class="px-6 py-4 whitespace-nowrap font-semibold">
+                    $ {{ $total }}
+                </td>
+            </tr>
+        </tbody>
+    </table>
--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
+ 
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-purple-800 dark:text-gray-200 leading-tight">
             {{ __('Pedidos') }}
@@ -15,7 +16,7 @@
                         <label for="hospital_id" class="font-medium">Filtrar por Hospital:</label>
                         <select name="hospital_id" id="hospital_id" class="border rounded-md px-2 py-1">
                             <option value="">Todos los hospitales</option>
-                            @foreach($hospitals as $hospital)
+                            @foreach ($hospitals as $hospital)
                                 <option value="{{ $hospital->id }}"
                                     {{ request('hospital_id') == $hospital->id ? 'selected' : '' }}>
                                     {{ $hospital->name }}
@@ -25,8 +26,8 @@
                     </div>
                     <div>
                         <label for="filter_date" class="font-medium">Filtrar por Fecha:</label>
-                        <input type="date" name="filter_date" id="filter_date"
-                            value="{{ request('filter_date') }}" class="border rounded-md px-2 py-1">
+                        <input type="date" name="filter_date" id="filter_date" value="{{ request('filter_date') }}"
+                            class="border rounded-md px-2 py-1">
                     </div>
                     <div>
                         <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-md">
@@ -40,29 +41,35 @@
                 <table class="min-w-full divide-y divide-gray-200">
                     <thead class="bg-gray-50">
                         <tr>
-                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            <th scope="col"
+                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                 ID
                             </th>
-                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            <th scope="col"
+                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                 Hospital ID
                             </th>
-                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            <th scope="col"
+                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                 Deadline
                             </th>
-                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            <th scope="col"
+                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                 Observations
                             </th>
-                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            <th scope="col"
+                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                 Status
                             </th>
-                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            <th scope="col"
+                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                 Total
                             </th>
                             <th scope="col" class="px-6 py-3">
                                 <span class="sr-only">Detalles</span>
                             </th>
                         </tr>
-                    </thead>                    
+                    </thead>
                     <tbody class="bg-white divide-y divide-gray-200">
                         @foreach ($pedidos as $pedido)
                             <tr>
@@ -89,73 +96,123 @@
                                     {{ $pedido->total }}
                                 </td>
                                 <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                    <a type="button" class="text-orange-700 detalles-btn" data-id="{{ $pedido->id }}">
+                                    <a type="button" class="text-orange-700 detalles-btn"
+                                        data-id="{{ $pedido->id }}">
                                         Detalles
                                     </a>
+
+
                                 </td>
                             </tr>
                         @endforeach
                     </tbody>
                 </table>
-                
+
             </div>
 
             <div class="mt-4">
                 {{ $pedidos->appends(request()->except('page'))->links() }}
             </div>
         </div>
+
+        
     </div>
-    <div id="detalleModal" class="modal hidden fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+    {{-- <div id="detalleModal" class="modal hidden fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+        
         <div class="modal-content bg-white rounded-lg p-6">
             <!-- Aquí se cargarán los detalles del pedido mediante AJAX -->
         </div>
+    </div> --}}
+    <div id="detalleModal" tabindex="-1" aria-hidden="true"
+    class="modal hidden fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+    <div class="relative w-full max-w-2xl max-h-full">
+        <!-- Modal content -->
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <!-- Modal header -->
+            <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                    Detalles del pedido
+                </h3>
+                <button type="button"
+                    class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                    data-modal-hide="defaultModal">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                    </svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+            </div>
+            <!-- Modal body -->
+            <div class="modal-content bg-white rounded-lg p-6">
+                <!-- Aquí se cargarán los detalles del pedido mediante AJAX -->
+            </div>
+           
+        </div>
     </div>
+</div>
+    
     <script>
-    document.addEventListener('DOMContentLoaded', function () {
-        const detallesButtons = document.querySelectorAll('.detalles-btn');
-        const modal = document.getElementById('detalleModal');
-        const modalContent = modal.querySelector('.modal-content');
-    
-        detallesButtons.forEach(button => {
-            button.addEventListener('click', function (event) {
-                event.preventDefault(); // Evita el comportamiento predeterminado del enlace/button
-                
-                const pedidoId = button.getAttribute('data-id');
-                
-                // Lógica para cargar los detalles del pedido en el modal (puedes usar AJAX)
-                cargarDetallesPedido(pedidoId);
-                
-                // Muestra el modal
-                modal.classList.remove('hidden');
+        document.addEventListener('DOMContentLoaded', function() {
+            const detallesButtons = document.querySelectorAll('.detalles-btn');
+            const modal = document.getElementById('detalleModal');
+            const modalContent = modal.querySelector('.modal-content');
+
+            detallesButtons.forEach(button => {
+                button.addEventListener('click', function(event) {
+                    event
+                .preventDefault(); // Evita el comportamiento predeterminado del enlace/button
+
+                    const pedidoId = button.getAttribute('data-id');
+
+                    // Lógica para cargar los detalles del pedido en el modal (puedes usar AJAX)
+                    cargarDetallesPedido(pedidoId);
+
+                    // Muestra el modal
+                    modal.classList.remove('hidden');
+                });
             });
-        });
-    
-        // Cierra el modal al hacer clic fuera de él
-        modal.addEventListener('click', function (event) {
-            if (event.target === modal) {
-                modal.classList.add('hidden');
+
+            // Cierra el modal al hacer clic fuera de él
+            modal.addEventListener('click', function(event) {
+                if (event.target === modal) {
+                    modal.classList.add('hidden');
+                }
+            });
+
+            function cargarDetallesPedido(pedidoId) {
+                // Utiliza AJAX para cargar los detalles del pedido en el modalContent
+                // Por ejemplo:
+                modalContent.innerHTML = '<p>Cargando detalles...</p>';
+                // Realiza la solicitud AJAX para cargar los detalles del pedido
+                $.ajax({
+                    url: '/admin/orders/' + pedidoId + '/detalle',
+                    method: 'GET',
+                    success: function(response) {
+                        console.log(response);
+                        modalContent.innerHTML = response;
+                    },
+                    error: function() {
+                        modalContent.innerHTML = '<p>Error al cargar los detalles.</p>';
+                    }
+                });
             }
         });
-    
-        function cargarDetallesPedido(pedidoId) {
-            // Utiliza AJAX para cargar los detalles del pedido en el modalContent
-            // Por ejemplo:
-            modalContent.innerHTML = '<p>Cargando detalles...</p>';
-            // Realiza la solicitud AJAX para cargar los detalles del pedido
-            $.ajax({
-                url: '/admin/orders/' + pedidoId + '/detalle',
-                method: 'GET',
-                success: function(response) {
-                    console.log(response);
-                    modalContent.innerHTML = response;
-                },
-                error: function() {
-                    modalContent.innerHTML = '<p>Error al cargar los detalles.</p>';
+
+        //cerrar modal
+        var closeButton = document.querySelector('[data-modal-hide="defaultModal"]');
+
+        // Agrega un evento de clic al botón de cerrar
+        if (closeButton) {
+            closeButton.addEventListener('click', function() {
+                // Encuentra el modal por su ID y ocúltalo
+                var modal = document.getElementById('detalleModal');
+                if (modal) {
+                    modal.classList.add('hidden');
                 }
             });
         }
-    });
     </script>
-    
-</x-app-layout>
 
+</x-app-layout>
