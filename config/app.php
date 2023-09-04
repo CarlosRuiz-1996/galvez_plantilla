ade887d950834b6c5cb7473eed55e9f3c910a469
Carlos Ruiz <131738990+CarlosRuiz-1996@users.noreply.github.com>
2023-09-04 15:22:36 -0600

    instalo liberia por composer para excel

--- config/app.php
+++ config/app.php
@@ -185,6 +185,8 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
+
     ])->toArray(),
 
 ];
